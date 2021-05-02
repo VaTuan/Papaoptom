daad0d7dc600aec94b82050e4f57a2980858244c
Vu Minh Khoa <vuminhkhoa97@gmail.com>
2021-05-02 15:34:25 +0700

    add env

--- next.config.js
+++ next.config.js
@@ -1,19 +1,31 @@
 const withPlugins = require("next-compose-plugins");
 const withOptimizedImages = require("next-optimized-images");
 
-
-
 // next.js configuration
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/shoes",
-        permanent: true,
-      },
-    ];
-  },
+
+const nextConfig = (phase, { defaultConfig }) => {
+  let publicEnv = {};
+  for (let key in process.env) {
+    if (key.includes("NEXT_PUBLIC")) {
+      publicEnv = {
+        ...publicEnv,
+        key: process.env[key],
+      };
+    }
+  }
+  return {
+    ...defaultConfig,
+    publicRuntimeConfig: publicEnv,
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/shoes",
+          permanent: true,
+        },
+      ];
+    },
+  };
 };
 
 module.exports = withPlugins([withOptimizedImages], nextConfig);
