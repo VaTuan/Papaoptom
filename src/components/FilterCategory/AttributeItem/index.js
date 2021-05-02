63c6423dcdb8939be1f0b42b085e88bcdb13a54a
tuanva <vuanhtuan743@gmail.com>
2021-05-02 16:52:34 +0700

    update api endpoint

--- src/components/FilterCategory/AttributeItem/index.js
+++ src/components/FilterCategory/AttributeItem/index.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { memo } from "react";
 
-function AttributeItem() {
-  return <div></div>;
+function AttributeItem(props) {
+  return (
+    <div className="custom-control custom-checkbox">
+      <input
+        type="checkbox"
+        className="custom-control-input"
+        id={`customCheck1-${product.slug}`}
+        checked={product.checked}
+        onChange={() => handleChange(product.slug)}
+      />
+      <label
+        className="custom-control-label"
+        htmlFor={`customCheck1-${product.slug}`}
+      >
+        {product.name}
+      </label>
+    </div>
+  );
 }
 
-export default AttributeItem;
+export default memo(AttributeItem);
